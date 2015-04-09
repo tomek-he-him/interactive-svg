7704c39a4111258a656eb64e8c2eefa04afc331f
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-10 00:20:41 +0200

    Update code style

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,37 @@
-/*jshint esnext: false, globalstrict: true */
-"use strict";
+'use strict';
 
-var gulp = require("gulp-help")(require("gulp"));
-var del = require("del");
-var webpack = require("webpack");
+var gulp = require('gulp-help')(require('gulp'));
+var del = require('del');
+var webpack = require('webpack');
 
-
-var common =
-  { target: "dist"
-  };
-
+var common = {
+  target: 'dist'
+};
 
 // `gulp build`
 // -------------------------------------------------------------------------------------------------
-
-gulp.task("build"
-  , "Compile everything."
-  , ["clean", "scripts"]
-  );
-
+gulp.task('build',
+  'Compile everything.',
+  ['clean', 'scripts']
+);
 
 // `gulp scripts`
 // -------------------------------------------------------------------------------------------------
+gulp.task('scripts',
+  'Compile scripts with webpack.',
+  ['scripts:clean'],
+  function(done) {
+    webpack(require('./webpack.config.js'), done);
+  }
+);
 
-gulp.task("scripts"
-  , "Compile scripts with webpack."
-  , ["scripts:clean"]
-  , function (done) {
-    webpack(require("./webpack.config.js"), done);
-    }
-  );
-
-gulp.task("scripts:clean", false, function (done) {
+gulp.task('scripts:clean', false, function(done) {
   del(common.target, done);
-  });
-
+});
 
 // `gulp clean`
 // -------------------------------------------------------------------------------------------------
-
-gulp.task("clean"
-  , "Remove all built files."
-  , ["scripts:clean"]
-  );
+gulp.task('clean',
+  'Remove all built files.',
+  ['scripts:clean']
+);
