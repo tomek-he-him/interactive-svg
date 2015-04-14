31d0e9f5e0a5f4192fd478620084961ebd391919
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-15 00:48:52 +0200

    Add a test plugin

--- source/scripts/pluginRegistry.js
+++ source/scripts/pluginRegistry.js
@@ -1,2 +1,6 @@
 export const rendererPlugins = [];
 export const intentPlugins = [];
+
+rendererPlugins.push((component) => {
+  component.attributeChannel.on('scale', console.log.bind(console));
+  });
