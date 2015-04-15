987d13d296be36810136f2180cfcbecff0e0498f
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-15 09:12:38 +0200

    Update vNodify

--- source/scripts/tools/vNodify.js
+++ source/scripts/tools/vNodify.js
@@ -1,17 +1,14 @@
 import hashifyAttributes from './hashifyAttributes';
 
 let vNode = {
-  tagName: 'div',
+  tagName: 'DIV',
   children: [],
   type: 'VirtualNode',
   version: '2'
 };
 
 export default function vNodify(element) {
-  return Object.assign(
-    Object.create(vNode),
-    {
-      properties: hashifyAttributes(element)
-    }
-  );
+  return Object.assign(Object.create(vNode), {
+    properties: hashifyAttributes(element)
+  });
 }
