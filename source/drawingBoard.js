9361f9d630649bd4c09b51c2f558a22e077196a9
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-17 23:25:24 +0200

    Create shadow DOM structure

--- source/drawingBoard.js
+++ source/drawingBoard.js
@@ -7,21 +7,30 @@ import defaultPlugins from './drawingBoard/defaultPlugins';
 const HTMLDrawingBoardElement =
   document.registerElement('drawing-board', {
     prototype: Object.assign(
-      Object.create(HTMLElement),
+      Object.create(HTMLElement.prototype),
       {
         createdCallback() {
 
+          // Create internal shadow DOM.
+          const root = this;
+          const shadowRoot = root.createShadowRoot();
+          const viewport = document.createElement('svg');
+          const content = document.createElement('content');
+          viewport.appendChild(content);
+          shadowRoot.appendChild(viewport);
+
+          // Save references to the DOM.
+          const elements = { root, shadowRoot, viewport };
+
           // Initialize the model and view.
-          const model = _model(this);
-          const view = _view(this);
+          const model = _model(this, elements);
+          const view = _view(this, elements);
 
           // Initialize default plugins.
           defaultPlugins.forEach((plugin) => plugin(model, view));
 
           // Export data.
-          Object.assign(this,
-            { model, view }
-          );
+          Object.assign(this, { model, view, elements });
         },
 
         attributeChangedCallback: _model.attributeChangedCallback
