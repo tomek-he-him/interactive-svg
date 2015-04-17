b9573ede7a5ebea5a29787a8279971dac8cdf01b
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-17 23:10:13 +0200

    Get rid of the HTML bundle

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,16 @@
 'use strict';
 
-var fs = require('fs');
-
 var del = require('del');
 var webpack = require('webpack');
 
 var gulp = require('gulp-help')(require('gulp'));
-var mustache = require('gulp-mustache');
-var rename = require('gulp-rename');
 var webpackConfig = require('./webpack.config.js');
-
-function fileToString(path) {
-  return fs.readFileSync(path, { encoding: 'utf-8' }); }
 
 // `gulp build`
 // -------------------------------------------------------------------------------------------------
 gulp.task('build',
   'Compile everything.',
-  [ 'clean', 'scripts', 'bundle' ]);
+  [ 'clean', 'scripts' ]);
 
 // `gulp scripts`
 // -------------------------------------------------------------------------------------------------
@@ -29,21 +22,6 @@ gulp.task('scripts',
 
 gulp.task('scripts:clean', false, function(done) {
   del(webpackConfig.output.path + '/**/*.{js,js.map}', done); });
-
-// `gulp bundle`
-// -------------------------------------------------------------------------------------------------
-gulp.task('bundle',
-  'Compile a HTML bundle.',
-  [ 'bundle:clean' ],
-  function() {
-    return gulp.src('build/drawing-board.html.mustache')
-      .pipe(mustache({
-        templates: fileToString('source/templates.html') }))
-      .pipe(rename({ extname: '' }))
-      .pipe(gulp.dest(webpackConfig.output.path)); });
-
-gulp.task('bundle:clean', false, function(done) {
-  del(webpackConfig.output.path + '/**/*.html', done); });
 
 // `gulp clean`
 // -------------------------------------------------------------------------------------------------
