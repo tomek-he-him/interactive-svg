c228d12010f612f1e9b73fb84766316f5b9d2786
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-18 23:01:43 +0200

    Update import paths

--- source/drawingBoard/view.js
+++ source/drawingBoard/view.js
@@ -1,9 +1,9 @@
 import stereo from 'stereo';
 
 import updateTransformation from './view/updateTransformation';
-import applyTransformations from './applyTransformations';
-import vPatchify from '../tools/vPatchify';
-import updateElement from '../tools/updateElement';
+import applyTransformations from './view/applyTransformations';
+import vPatchify from './tools/vPatchify';
+import updateElement from './tools/updateElement';
 
 export default function view (viewportElement) {
 
