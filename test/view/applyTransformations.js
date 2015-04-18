f1426afc9f0297a489f144bfb28ee2fbd90fb7c7
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-18 22:57:44 +0200

    Specify applyTransformations

---
+++ test/view/applyTransformations.js
@@ -0,0 +1,88 @@
+/* jshint elision: true */
+import test from 'tape-catch';
+
+import applyTransformations from
+  '../../source/drawingBoard/view/applyTransformations'
+;
+
+
+test('view/applyTransformations:  ' +
+  'Applies view box transformation functions.', (is) => {
+
+    is.deepEqual(
+      applyTransformations([
+        () => [1, 2, 3, 4]
+      ]).viewBoxUpdate,
+      '1 2 3 4',
+      'applies a single function'
+    );
+
+    is.deepEqual(
+      applyTransformations([
+        (() => [1, 2, 3, 4]),
+        ((array) => array.map((coord) => coord * 2))
+      ]).viewBoxUpdate,
+      '2 4 6 8',
+      'applies two functions'
+    );
+
+    is.deepEqual(
+      applyTransformations([
+        (() => [1, 2, 3, 4]),
+        ((array) => array.map((coord) => coord * 2)),
+        ((array) => array.map((coord) => coord + 10))
+      ]).viewBoxUpdate,
+      '12 14 16 18',
+      'applies three functions'
+    );
+
+    is.deepEqual(
+      applyTransformations([
+        (() => [1, 2, 3, 4]),
+        ,,,,
+        ((array) => array.map((coord) => coord * 2)),
+        ,,
+      ]).viewBoxUpdate,
+      '2 4 6 8',
+      'works for arrays with holes'
+    );
+
+    is.end();
+  }
+);
+
+
+test('view/applyTransformations:  ' +
+  'Errors when it becomes a wrong result.', (is) => {
+
+    is.ok(applyTransformations([]).error,
+      'on an empty list of transformations'
+    );
+
+    is.ok(applyTransformations([
+      () => /anything/
+    ]).error,
+      'when it becomes a non-array'
+    );
+
+    is.ok(applyTransformations([
+      () => [1, 2, 3]
+    ]).error,
+      'when it becomes too short an array'
+    );
+
+    is.ok(applyTransformations([
+      () => [1, 2, 3, 4, 5]
+    ]).error,
+      'when it becomes too long an array'
+    );
+
+    is.throws(applyTransformations([
+      () => [1, 2, 3, 'four']
+    ]).error,
+      'throwing when it becomes an array with `NaN`s'
+    );
+
+    is.end();
+  }
+);
