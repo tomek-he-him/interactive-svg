515b78a37f9b2efb454ea0818fc24246186c5609
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-20 21:58:58 +0200

    Add a style

--- source/drawingBoard.js
+++ source/drawingBoard.js
@@ -4,6 +4,10 @@ import _model from './drawingBoard/model';
 import _view from './drawingBoard/view';
 import defaultPlugins from './drawingBoard/defaultPlugins';
 
+const SVG_NS = 'http://www.w3.org/2000/svg';
+const XMLNS_NS = 'http://www.w3.org/2000/xmlns/';
+const XLINK_NS = 'http://www.w3.org/1999/xlink';
+
 const HTMLDrawingBoardElement =
   document.registerElement('drawing-board', {
     prototype: Object.assign(
@@ -14,15 +18,15 @@ const HTMLDrawingBoardElement =
           // Create internal shadow DOM.
           const root = this;
           const shadowRoot = root.createShadowRoot();
-          const viewport = document.createElementNS(
-            'http://www.w3.org/2000/svg',
-            'svg'
-          );
-          viewport.setAttributeNS(
-            'http://www.w3.org/2000/xmlns/',
-            'xmlns:xlink', 'http://www.w3.org/1999/xlink'
-          );
+
+          const style = document.createElement('style');
+          style.textContent = '#v{overflow:hidden}';
+          shadowRoot.appendChild(style);
+
+          const viewport = document.createElementNS(SVG_NS, 'svg');
           const content = document.createElement('content');
+          viewport.setAttributeNS(XMLNS_NS, 'xmlns:xlink', XLINK_NS);
+          viewport.setAttribute('id', 'v');
           viewport.appendChild(content);
           shadowRoot.appendChild(viewport);
 
