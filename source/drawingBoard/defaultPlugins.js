84c809881ac7708df46a77de57969fae18874cbc
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-22 01:06:04 +0200

    Add the plugin `viewport`

--- source/drawingBoard/defaultPlugins.js
+++ source/drawingBoard/defaultPlugins.js
@@ -1,4 +1,37 @@
+import vPatchify from './tools/vPatchify';
+
+
 const defaultPlugins = new Set([
+  function viewport(model, view) {
+    model.attributeChanges.when('viewport', (vNode) => {
+      const [width, height] = vNode.properties.viewport
+        .split('×')
+        .map(Number)
+      ;
+
+      // Update the viewport’s style.
+      view.attributeUpdates.emit('update', vPatchify({
+        style: (
+          'width: ' + width + 'px;' +
+          'height: ' + height + 'px;'
+        ),
+        width,
+        height
+      }));
+
+      // Update the viewport’s viewBox.
+      view.viewBoxTransformations.emit('update', {
+        priority: 0,
+        transformFunction: () => [
+          -(width / 2),
+          -(height / 2),
+          width / 2,
+          height / 2
+        ]
+      });
+    });
+  },
+
   function scale(model) {
     model.attributeChanges.when('scale', (...args) => {
       console.log('Scale plugin gets:', ...args);
