23ed9c8c44cc34dc33790afdd541b106c812ac27
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-23 00:06:44 +0200

    Implement the scale plugin

--- source/drawingBoard/defaultPlugins.js
+++ source/drawingBoard/defaultPlugins.js
@@ -32,9 +32,13 @@ const defaultPlugins = new Set([
     });
   },
 
-  function scale(model) {
-    model.attributeChanges.when('scale', (...args) => {
-      console.log('Scale plugin gets:', ...args);
+  function scale(model, view) {
+    model.attributeChanges.when('scale', (vNode) => {
+      const scale = Number(vNode.properties.scale);
+      view.viewBoxTransformations.emit('update', {
+        priority: 100,
+        transformFunction: (coords) => coords.map((coord) => coord / scale)
+      });
     });
   }
 ]);
