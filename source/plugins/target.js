8d8a15b6ae3d1fd0a16e3cda03944bc323dc1979
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-24 14:56:39 +0200

    Better error handling in the target plugin

--- source/plugins/target.js
+++ source/plugins/target.js
@@ -1,17 +1,31 @@
+const proto = {
+  priority: 10
+};
+
 export default function target(model, view) {
   model.attributeChanges.when('target', (vNode) => {
     const [x, y] = vNode.properties.target
       .split(',')
       .map(Number)
     ;
-    view.viewBoxTransformations.emit('update', {
-      priority: 10,
+    const update = Object.create(proto);
+    const {emit} = view.viewBoxTransformations;
+
+    if (![x, y].every(Number.isFinite)) {
+      emit('error', new Error(
+        'drawingBoard.target: The <drawing-board> attribute `target` should ' +
+        'match the form "`{Number} x`, `{Number} y`".'
+      ));
+      emit('update', Object.assign(update, {transformFunction: null}));
+    }
+
+    else emit('update', Object.assign(update, {
       transformFunction: (coords) => [
         coords[0] + x,
         coords[1] + y,
         coords[2],
         coords[3]
       ]
-    });
+    }));
   });
 }
