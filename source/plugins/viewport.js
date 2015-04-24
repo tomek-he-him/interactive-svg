b6deceaba16e14eb502128f2f500eaabe21da3df
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-24 15:15:46 +0200

    Better error handling in the viewport plugin

--- source/plugins/viewport.js
+++ source/plugins/viewport.js
@@ -4,13 +4,21 @@ const dimension = /^\s*(?:width|height):/;
 
 export default function viewport(model, view, elements) {
   model.attributeChanges.when('viewport', (vNode) => {
-    const [width, height] = vNode.properties.viewport
-      .split('×')
-      .map(Number)
-    ;
     const currentStyle = elements.viewport.getAttribute('style');
 
-    // Update the viewport’s style.
+    // Validate the attribute.
+    const [width, height] = vNode.properties.viewport
+      .split(/[×x]/)
+      .map(Number)
+    ;
+    if (![width, height].every(Number.isFinite)) {
+      view.viewBoxTransformations.emit('error', new Error(
+        'drawingBoard.viewport: The <drawing-board> attribute `viewport` ' +
+        'should match the form "`{Number} width` × `{Number} height`".'
+      ));
+    }
+
+    // Update the viewport’s style, dimensions and viewBox.
     view.attributeUpdates.emit('update', vPatchify({
       style: (currentStyle ?
         currentStyle
@@ -27,6 +35,7 @@ export default function viewport(model, view, elements) {
       viewBox: `${-(width / 2)} ${-(height / 2)} ${width} ${height}`
     }));
 
+    // Recalculate the viewBox.
     view.viewBoxTransformations.emit('touch');
   });
 }
