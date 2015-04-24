e78a13646a83efef045370bc6b5b236405867f2a
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-24 15:05:38 +0200

    Better error handling in the scale plugin

--- source/plugins/scale.js
+++ source/plugins/scale.js
@@ -1,9 +1,26 @@
+const proto = {
+  priority: 100,
+  transformFunction: null
+};
+
 export default function scale(model, view) {
   model.attributeChanges.when('scale', (vNode) => {
+    const emptyUpdate = Object.create(proto);
+    const {emit} = view.viewBoxTransformations;
+
+    // Validate the attribute.
     const scale = Number(vNode.properties.scale);
-    view.viewBoxTransformations.emit('update', {
-      priority: 100,
+    if (!Number.isFinite(scale)) {
+      emit('error', new Error(
+        'drawingBoard.scale: The <drawing-board> attribute `scale` should ' +
+        'be a `{Number}`.'
+      ));
+      emit('update', emptyUpdate);
+    }
+
+    // Inject the viewBox transformation.
+    else emit ('update', Object.assign(emptyUpdate, {
       transformFunction: (coords) => coords.map((coord) => coord / scale)
-    });
+    }));
   });
 }
